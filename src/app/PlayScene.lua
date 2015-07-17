bf7d565e7040cce2ef7b5558203655e0422ae039
klstudio <jianzhang.zj@gmail.com>
2015-07-17 17:03:26 -0400

    lua coding ...

--- src/app/PlayScene.lua
+++ src/app/PlayScene.lua
@@ -1,15 +1,17 @@
 
 local PlayScene = class("PlayScene", cc.load("mvc").ViewBase)
+local ninjia = {}
 
 function PlayScene:onCreate()
-    -- create game view and add it to stage
-    --[[
-    self.gameView_ = GameView:create()
-        :addEventListener(GameView.events.PLAYER_DEAD_EVENT, handler(self, self.onPlayerDead))
-        :start()
-        :addTo(self)
-    --]]
+    local s = cc.Director:getInstance():getWinSize()
+    -- background
     cc.LayerColor:create(cc.c4b(50,50,50,255)):addTo(self)
+    local cache = cc.SpriteFrameCache:getInstance()
+    cache:addSpriteFrames("ninjia.plist")
+
+    ninjia.sprite = cc.Sprite:createWithSpriteFrame( cache:getSpriteFrame("Idle__001.png") )
+    ninjia.sprite:setPosition( cc.p( s.width/2-80, s.height/2) )
+    self:addChild(ninjia.sprite)
 
 end
 
