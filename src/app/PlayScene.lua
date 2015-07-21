6d19405dfe46b83dbe5216f4c7fa5c5cda85846c
klstudio <jianzhang.zj@gmail.com>
2015-07-21 17:19:28 -0400

    continue ninjia module...

--- src/app/PlayScene.lua
+++ src/app/PlayScene.lua
@@ -10,7 +10,8 @@ function PlayScene:onCreate()
 
     local ninjia1 = nj.new(1)
     local ninjia2 = nj.new(2)
-    local ninjia3 = nj.new(2)
+    local ninjia3 = nj.new(3)
+    local ninjia4 = nj.new(4)
 
     self:addChild( ninjia1.sprite )
     nj.setPosition( ninjia1, cc.p( s.width/2-280, s.height/2) )
@@ -19,11 +20,15 @@ function PlayScene:onCreate()
     nj.setPosition( ninjia2, cc.p( s.width/2-80, s.height/2) )
 
     self:addChild( ninjia3.sprite )
-    nj.setPosition( ninjia3, cc.p( s.width/2+180, s.height/2) )
+    nj.setPosition( ninjia3, cc.p( s.width/2+80, s.height/2) )
+
+    self:addChild( ninjia4.sprite )
+    nj.setPosition( ninjia4, cc.p( s.width/2+180, s.height/2) )
 
     nj.runAnimation( ninjia1, "Idle" )
     nj.runAnimation( ninjia2, "Run" )
     nj.runAnimation( ninjia3, "Throw" )
+    nj.runAnimation( ninjia4, "Dash" )
 end
 
 return PlayScene
--- src/app/ninjia.lua
+++ src/app/ninjia.lua
@@ -1,25 +1,40 @@
 local M={}
 
-local cache = cc.SpriteFrameCache:getInstance()
-cache:addSpriteFrames("ninjia.plist")
 
 local anim = {}
 
-local function initAnimation(name, getframe)
+-- each state must have corresponding animation
+local states = {
+    ["Idle"] = { v=cc.p(0,0), a=cc.p(0,0) },
+    ["Run"] = { v=cc.p(1,0), a=cc.p(0,0)},
+    ["Dash"] = { v=cc.p(3,0), a=cc.p(0,0)},
+}
+
+local cache = cc.SpriteFrameCache:getInstance()
+cache:addSpriteFrames("ninjia.plist")
+
+local function initAnimation(name, getframe, interval)
     local frames={}
+    interval = interval or 0.1
+
     for i = 0,9 do
         local frame = getframe(i)
         frames[i] = frame
-        print(frame)
+        --print(frame)
     end
-    local animation = cc.Animation:createWithSpriteFrames( frames, 0.1 )
-    anim[name] = {frames=frames, animation=animation}
+    print("interval = ", interval)
+    local animation = cc.Animation:createWithSpriteFrames( frames, interval )
+    anim[name] = {frames=frames, animation=animation}   -- table need be explicitly created
 end
 
+--ninjia = { id, sprite, velocity, a, g }
 function M.new( id )
-   local ninjia={}
-   ninjia.sprite = cc.Sprite:createWithSpriteFrame( cache:getSpriteFrame("Idle__001.png") )
-   ninjia.id = id
+   local ninjia={sprite = cc.Sprite:createWithSpriteFrame( cache:getSpriteFrame("Idle__001.png") ),
+                 id = id,
+                 state = "Idle",
+                 v = cc.p(0, 0),  -- velocity
+                 a = cc.p(0,0),   -- acceleration
+                }
    return ninjia
 end
 
@@ -30,6 +45,15 @@ end
 function M.runAnimation( ninjia, state )
     ninjia.sprite:runAction( cc.RepeatForever:create( cc.Animate:create(anim[state].animation) ) )
 end
+
+function M.setState(ninjia, state)
+    ninjia.sprite:stopAllActions()
+    M.runAnimation( ninjia, state )
+    ninjia.v = states[state].v
+    ninjia.a = states[state].a
+end
+
+
 
 initAnimation("Idle", function (i) 
                         return cache:getSpriteFrame( string.format("Idle__%03d.png", i) )
@@ -42,6 +66,11 @@ initAnimation("Run", function (i)
 initAnimation("Throw", function (i) 
                         return cache:getSpriteFrame( string.format("Throw__%03d.png", i) )
                        end
+             )
+initAnimation("Dash", function (i) 
+                        return cache:getSpriteFrame( string.format("Run__%03d.png", i) )
+                       end,
+               0.05
              )
 return M
       
